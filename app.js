94c1cb73a75768f77bb38562545bb5bc78574697
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-02 11:50:52 +0500

    Add work

---
+++ app.js
@@ -0,0 +1,47 @@
+var num1= +prompt("Enter Your First Number");
+var num2= +prompt("Enter Your Second Number");
+var sign= prompt("Enter Operator");
+
+if(sign=="+"){
+    alert(num1 + num2);
+}
+else if(sign=="-") {
+    alert (num1 - num2);
+}
+
+else if(sign=="*") {
+    alert (num1 * num2);
+}
+
+else if(sign=="/") {
+    alert (num1 / num2);
+}
+
+
+
+else {
+    alert ("Invalid Operator");
+}
+
+var num1= +prompt("Enter Your First Number");
+var num2= +prompt("Enter Your Second Number");
+var sign= prompt("Enter Operator");
+
+if(sign=="+"){
+    console.log(num1 + num2);
+}
+else if(sign=="-") {
+    console.log (num1 - num2);
+}
+
+else if(sign=="*") {
+    console.log (num1 * num2);
+}
+
+else if(sign=="/") {
+    console.log (num1 / num2);
+}
+
+else {
+    console.log ("Invalid Operator");
+}
